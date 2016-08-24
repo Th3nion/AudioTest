30b199fa9a7520498c189c4c225b3ee71740e814
Th3nion <steaker90@gmail.com>
2016-08-24 07:25:55 +0200

    remove untitled

--- untitled/src/TransfertMessageInterface.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.server.UID;
-
-/**
- * Created by Thenion on 07/12/2015.
- */
-public interface TransfertMessageInterface {
-    public void transfertMessage(byte[] buffer, UID id);
-}
